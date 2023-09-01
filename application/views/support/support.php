9c97f4a2b72b8fdca042df682b3697ae807b0a5f
samue <samue@DESKTOP-RFSK8KP>
2023-09-01 15:13:16 -0300

    Correção de tamnho das fontes e disposição do texto na tela

--- application/views/support/support.php
+++ application/views/support/support.php
@@ -4,26 +4,26 @@
     <?php $this->load->view('elements/sidebar'); ?>
     <div class="content">
         <div class="row">
-            <div class="col-md-12">
+            <div class="col-md-10 mx-auto text-center">
                 <div class="card">
                     <div class="card-header">
-                       <h3>Central de Suporte - Sistema de Regularização Fundiária Urbana (REURB)</h3>
+                       <h3 style="font-size: 26px;">Central de Suporte - Sistema de Regularização Fundiária Urbana (REURB)</h3>
                     </div>
                     <div class="card-body">
                         <div class="row">
-                            <div class="col-md-6 text-center">
+                            <div class="col-md-12 text-center">
                                 <div class="narrow-text">
-                                    <h5>Bem-vindo à nossa Central de Suporte para o Sistema de Regularização Fundiária Urbana (REURB)!</h5>
+                                    <h5 style="font-size: 24px;">Bem-vindo à nossa Central de Suporte para o Sistema de Regularização Fundiária Urbana (REURB)!</h5>
 
-                                    <p>Nós entendemos o quão importante é simplificar e agilizar o processo de regularização fundiária urbana. Se você tiver alguma pergunta, necessitar de orientação ou enfrentar algum desafio ao utilizar nosso sistema, nossa equipe de suporte altamente capacitada está pronta para lhe fornecer a assistência necessária.</p>
+                                    <p style="font-size: 16px;">Nós entendemos o quão importante é simplificar e agilizar o processo de regularização fundiária urbana. Se você tiver alguma pergunta, necessitar de orientação ou enfrentar algum desafio ao utilizar nosso sistema, nossa equipe de suporte altamente capacitada está pronta para lhe fornecer a assistência necessária.</p>
 
-                                    <p>Estamos aqui para esclarecer dúvidas, fornecer instruções detalhadas sobre as etapas do processo REURB e resolver quaisquer problemas que possam surgir. Nossa missão é garantir que a sua experiência com o nosso sistema seja eficiente e sem complicações.</p>
+                                    <p style="font-size: 16px;">Estamos aqui para esclarecer dúvidas, fornecer instruções detalhadas sobre as etapas do processo REURB e resolver quaisquer problemas que possam surgir. Nossa missão é garantir que a sua experiência com o nosso sistema seja eficiente e sem complicações.</p>
 
-                                    <p>Você pode entrar em contato conosco utilizando as opções de atendimento listadas abaixo. Nosso compromisso é responder prontamente e oferecer soluções eficazes para suas necessidades específicas.</p>
+                                    <p style="font-size: 16px;">Você pode entrar em contato conosco utilizando as opções de atendimento listadas abaixo. Nosso compromisso é responder prontamente e oferecer soluções eficazes para suas necessidades específicas.</p>
 
-                                    <p>Agradecemos por escolher o Sistema Administrativo de Processo REURB e saiba que estamos dedicados a apoiar você em cada etapa deste processo fundamental.</p>
+                                    <p style="font-size: 16px;">Agradecemos por escolher o Sistema Administrativo de Processo REURB e saiba que estamos dedicados a apoiar você em cada etapa deste processo fundamental.</p>
 
-                                    <p><strong>Equipe de Suporte do Sistema Administrativo de Processo REURB</strong></p>
+                                    <p style="font-size: 18px;"><strong>Equipe de Suporte do Sistema Administrativo de Processo REURB</strong></p>
                                     <h6>(38) 99216-5773 - Onildo Rodrigues Lima</h6>
                                 </div>
                                 <!-- <img src="<?php echo base_url(); ?>assets/build/img/construcao.png" alt=""> -->
